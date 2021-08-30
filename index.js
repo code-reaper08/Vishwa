ea51e7942d4a4066718e30030982b47b32618221
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-08-30 22:50:47 +0530

    Added stylings for mewin

--- index.js
+++ index.js
@@ -6,3 +6,36 @@ const MyworkContent = document.querySelector('#Mywork-content');
 
 const contact = document.querySelector('#contact');
 const contactContent = document.querySelector('#contact-content');
+
+me.addEventListener('click', () => {
+    const meBox = new WinBox({
+        class: "crt boom",
+        title: 'Me.exe',
+        background: '#01ba06',
+        width: '400px',
+        height: '400px',
+        mount: meContent
+    })
+})
+
+Mywork.addEventListener('click', () => {
+    const WorkBox = new WinBox({
+        class: "crt",
+        title: 'Me.exe',
+        background: '#01ba06',
+        width: '400px',
+        height: '400px',
+        mount: MyworkContent
+    })
+})
+
+contact.addEventListener('click', () => {
+    const contactBox = new WinBox({
+        class: "crt",
+        title: 'Me.exe',
+        background: '#01ba06',
+        width: '400px',
+        height: '400px',
+        mount: contactContent
+    })
+})
