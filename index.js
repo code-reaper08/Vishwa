97cf7488b1c6bcd20af6a54e92e1ead67854f904
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-08-30 21:38:52 +0530

    Added nav content and background effects

---
+++ index.js
@@ -0,0 +1,8 @@
+const me = document.querySelector('#me');
+const meContent = document.querySelector('#me-content');
+
+const Mywork = document.querySelector('#Mywork');
+const MyworkContent = document.querySelector('#Mywork-content');
+
+const contact = document.querySelector('#contact');
+const contactContent = document.querySelector('#contact-content');
