3b35345428c6b0d50352e43f725a8af93805bd3b
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-08-31 23:30:04 +0530

    Optimizations for mobile added

--- index.js
+++ index.js
@@ -7,47 +7,148 @@ const MyworkContent = document.querySelector('#Mywork-content');
 const contact = document.querySelector('#contact');
 const contactContent = document.querySelector('#contact-content');
 
-me.addEventListener('click', () => {
-    const meBox = new WinBox({
-        class: "crt",
-        title: 'Me.exe',
-        background: '#01ba06',
-        width: '400px',
-        top: 10,
-        bottom: 10,
-        left: 100,
-        right: 100,
-        height: '400px',
-        mount: meContent
-    })
-})
+// me.addEventListener('click', () => {
+//     const meBox = new WinBox({
+//         class: "crt",
+//         title: 'Me.exe',
+//         background: '#01ba06',
+//         width: '400px',
+//         top: 10,
+//         bottom: 10,
+//         left: 100,
+//         right: 100,
+//         height: '400px',
+//         mount: meContent
+//     })
+// })
 
-Mywork.addEventListener('click', () => {
-    const WorkBox = new WinBox({
-        class: "crt",
-        title: 'Mywork.exe',
-        background: '#01ba06',
-        width: '400px',
-        top: 10,
-        bottom: 10,
-        left: 100,
-        right: 100,
-        height: '400px',
-        mount: MyworkContent
-    })
-})
+// Mywork.addEventListener('click', () => {
+//     const WorkBox = new WinBox({
+//         class: "crt",
+//         title: 'Mywork.exe',
+//         background: '#01ba06',
+//         width: '400px',
+//         top: 10,
+//         bottom: 10,
+//         left: 100,
+//         right: 100,
+//         height: '400px',
+//         mount: MyworkContent
+//     })
+// })
 
-contact.addEventListener('click', () => {
-    const contactBox = new WinBox({
-        class: "crt",
-        title: 'Contact.exe',
-        background: '#01ba06',
-        width: '400px',
-        top: 10,
-        bottom: 10,
-        left: 100,
-        right: 100,
-        height: '400px',
-        mount: contactContent
-    })
-})
+// contact.addEventListener('click', () => {
+//     const contactBox = new WinBox({
+//         class: "crt",
+//         title: 'Contact.exe',
+//         background: '#01ba06',
+//         width: '400px',
+//         top: 10,
+//         bottom: 10,
+//         left: 100,
+//         right: 100,
+//         height: '400px',
+//         mount: contactContent
+//     })
+// })
+
+
+// mobile optimizations.
+
+function Mobresizer(x) {
+    if (x.matches) {
+        me.addEventListener('click', () => {
+            const meBox = new WinBox({
+                class: "crt",
+                title: 'Me.exe',
+                background: '#01ba06',
+                width: '100px',
+                top: 10,
+                bottom: 10,
+                left: 5,
+                right: 5,
+                height: '400px',
+                mount: meContent
+            })
+        })
+        
+        Mywork.addEventListener('click', () => {
+            const WorkBox = new WinBox({
+                class: "crt",
+                title: 'Mywork.exe',
+                background: '#01ba06',
+                width: '400px',
+                top: 10,
+                bottom: 10,
+                left: 5,
+                right: 5,
+                height: '400px',
+                mount: MyworkContent
+            })
+        })
+        
+        contact.addEventListener('click', () => {
+            const contactBox = new WinBox({
+                class: "crt",
+                title: 'Contact.exe',
+                background: '#01ba06',
+                width: '400px',
+                top: 10,
+                bottom: 10,
+                left: 5,
+                right: 5,
+                height: '400px',
+                mount: contactContent
+            })
+        })
+    } else {
+        me.addEventListener('click', () => {
+            const meBox = new WinBox({
+                class: "crt",
+                title: 'Me.exe',
+                background: '#01ba06',
+                width: '400px',
+                top: 10,
+                bottom: 10,
+                left: 100,
+                right: 100,
+                height: '400px',
+                mount: meContent
+            })
+        })
+        
+        Mywork.addEventListener('click', () => {
+            const WorkBox = new WinBox({
+                class: "crt",
+                title: 'Mywork.exe',
+                background: '#01ba06',
+                width: '400px',
+                top: 10,
+                bottom: 10,
+                left: 100,
+                right: 100,
+                height: '400px',
+                mount: MyworkContent
+            })
+        })
+        
+        contact.addEventListener('click', () => {
+            const contactBox = new WinBox({
+                class: "crt",
+                title: 'Contact.exe',
+                background: '#01ba06',
+                width: '400px',
+                top: 10,
+                bottom: 10,
+                left: 100,
+                right: 100,
+                height: '400px',
+                mount: contactContent
+            })
+        })
+    }
+  }
+  
+  let mediaquery = window.matchMedia("(max-width: 390px)")
+  Mobresizer(mediaquery) 
+  mediaquery.addListener(Mobresizer)
