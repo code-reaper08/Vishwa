5888f8c1a7300431fa5c1cdc58e449c2b3c75051
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-09-03 21:58:32 +0530

    Added tech images

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const me = document.querySelector('#me');
-const meContent = document.querySelector('#me-content');
+const me = document.querySelector("#me");
+const meContent = document.querySelector("#me-content");
 
-const Mywork = document.querySelector('#Mywork');
-const MyworkContent = document.querySelector('#Mywork-content');
+const Mywork = document.querySelector("#Mywork");
+const MyworkContent = document.querySelector("#Mywork-content");
 
-const contact = document.querySelector('#contact');
-const contactContent = document.querySelector('#contact-content');
+const contact = document.querySelector("#contact");
+const contactContent = document.querySelector("#contact-content");
 
-const skills = document.querySelector('#skills');
-const skillscontent  = document.querySelector('#skills-content');
+const skills = document.querySelector("#skills");
+const skillscontent = document.querySelector("#skills-content");
 
-const switcher = document.getElementById('switch');
+const switcher = document.getElementById("switch");
 
 // me.addEventListener('click', () => {
 //     const meBox = new WinBox({
@@ -57,136 +57,135 @@ const switcher = document.getElementById('switch');
 //     })
 // })
 
-
 // mobile optimizations.
 
 function Mobresizer(x) {
-    if (x.matches) {
-        me.addEventListener('click', () => {
-            const meBox = new WinBox({
-                class: "crt",
-                title: 'Me.exe',
-                background: '#01ba06',
-                width: '250px',
-                top: 10,
-                bottom: 10,
-                left: 5,
-                right: 5,
-                height: '400px',
-                mount: meContent
-            })
-        })
-        
-        Mywork.addEventListener('click', () => {
-            const WorkBox = new WinBox({
-                class: "crt",
-                title: 'Mywork.exe',
-                background: '#01ba06',
-                width: '250px',
-                top: 10,
-                bottom: 10,
-                left: 5,
-                right: 5,
-                height: '400px',
-                mount: MyworkContent
-            })
-        })
-        
-        contact.addEventListener('click', () => {
-            const contactBox = new WinBox({
-                class: "crt",
-                title: 'Contact.exe',
-                background: '#01ba06',
-                width: '250px',
-                top: 10,
-                bottom: 10,
-                left: 5,
-                right: 5,
-                height: '400px',
-                mount: contactContent
-            })
-        })
+  if (x.matches) {
+    me.addEventListener("click", () => {
+      const meBox = new WinBox({
+        class: "crt",
+        title: "Me.exe",
+        background: "#01ba06",
+        width: "250px",
+        top: 10,
+        bottom: 10,
+        left: 5,
+        right: 5,
+        height: "400px",
+        mount: meContent,
+      });
+    });
 
-        skills.addEventListener('click', () => {
-            const meBox = new WinBox({
-                class: "crt",
-                title: 'Me.exe',
-                background: '#01ba06',
-                width: '250px',
-                top: 10,
-                bottom: 10,
-                left: 5,
-                right: 5,
-                height: '400px',
-                mount: skillscontent
-            })
-        })
-    } else {
-        me.addEventListener('click', () => {
-            const meBox = new WinBox({
-                class: "crt",
-                title: 'Me.exe',
-                background: '#01ba06',
-                width: '400px',
-                top: 10,
-                bottom: 10,
-                left: 100,
-                right: 100,
-                height: '400px',
-                mount: meContent
-            })
-        })
-        
-        Mywork.addEventListener('click', () => {
-            const WorkBox = new WinBox({
-                class: "crt",
-                title: 'Mywork.exe',
-                background: '#01ba06',
-                width: '400px',
-                top: 10,
-                bottom: 10,
-                left: 100,
-                right: 100,
-                height: '400px',
-                mount: MyworkContent
-            })
-        })
-        
-        contact.addEventListener('click', () => {
-            const contactBox = new WinBox({
-                class: "crt",
-                title: 'Contact.exe',
-                background: '#01ba06',
-                width: '400px',
-                top: 10,
-                bottom: 10,
-                left: 100,
-                right: 100,
-                height: '400px',
-                mount: contactContent
-            })
-        })
+    Mywork.addEventListener("click", () => {
+      const WorkBox = new WinBox({
+        class: "crt",
+        title: "Mywork.exe",
+        background: "#01ba06",
+        width: "250px",
+        top: 10,
+        bottom: 10,
+        left: 5,
+        right: 5,
+        height: "400px",
+        mount: MyworkContent,
+      });
+    });
 
-        skills.addEventListener('click', () => {
-            const meBox = new WinBox({
-                class: "crt",
-                title: 'Skills.exe',
-                background: '#01ba06',
-                width: '400px',
-                top: 10,
-                bottom: 10,
-                left: 100,
-                right: 100,
-                height: '400px',
-                mount: skillscontent
-            })
-        })
-    }
+    contact.addEventListener("click", () => {
+      const contactBox = new WinBox({
+        class: "crt",
+        title: "Contact.exe",
+        background: "#01ba06",
+        width: "250px",
+        top: 10,
+        bottom: 10,
+        left: 5,
+        right: 5,
+        height: "400px",
+        mount: contactContent,
+      });
+    });
+
+    skills.addEventListener("click", () => {
+      const meBox = new WinBox({
+        class: "crt",
+        title: "Me.exe",
+        background: "#01ba06",
+        width: "250px",
+        top: 10,
+        bottom: 10,
+        left: 5,
+        right: 5,
+        height: "400px",
+        mount: skillscontent,
+      });
+    });
+  } else {
+    me.addEventListener("click", () => {
+      const meBox = new WinBox({
+        class: "crt",
+        title: "Me.exe",
+        background: "#01ba06",
+        width: "400px",
+        top: 10,
+        bottom: 10,
+        left: 100,
+        right: 100,
+        height: "400px",
+        mount: meContent,
+      });
+    });
+
+    Mywork.addEventListener("click", () => {
+      const WorkBox = new WinBox({
+        class: "crt",
+        title: "Mywork.exe",
+        background: "#01ba06",
+        width: "400px",
+        top: 10,
+        bottom: 10,
+        left: 100,
+        right: 100,
+        height: "400px",
+        mount: MyworkContent,
+      });
+    });
+
+    contact.addEventListener("click", () => {
+      const contactBox = new WinBox({
+        class: "crt",
+        title: "Contact.exe",
+        background: "#01ba06",
+        width: "400px",
+        top: 10,
+        bottom: 10,
+        left: 100,
+        right: 100,
+        height: "400px",
+        mount: contactContent,
+      });
+    });
+
+    skills.addEventListener("click", () => {
+      const meBox = new WinBox({
+        class: "crt",
+        title: "Skills.exe",
+        background: "#01ba06",
+        width: "400px",
+        top: 10,
+        bottom: 10,
+        left: 100,
+        right: 100,
+        height: "400px",
+        mount: skillscontent,
+      });
+    });
   }
-  
-  let mediaquery = window.matchMedia("(max-width: 390px)")
-  Mobresizer(mediaquery) 
-  mediaquery.addListener(Mobresizer)
+}
+
+let mediaquery = window.matchMedia("(max-width: 390px)");
+Mobresizer(mediaquery);
+mediaquery.addListener(Mobresizer);
 
 // switcher.addEventListener('click', () => {
 
@@ -209,15 +208,13 @@ function Mobresizer(x) {
 //         }
 //     })
 
-switcher.addEventListener('click', () => {
-    const csssheet = document.getElementById('normalcss');
-    if(csssheet.href.match("css/global.css")) {
-        csssheet.href = "css/mono_global.css";
-        document.getElementById('switch').innerText = "Want CRT"
-    }
-    else {
-        csssheet.href = "css/global.css";
-        document.getElementById('switch').innerText = "Want MonoChrome"
-
-    }
-})
+switcher.addEventListener("click", () => {
+  const csssheet = document.getElementById("normalcss");
+  if (csssheet.href.match("css/global.css")) {
+    csssheet.href = "css/mono_global.css";
+    document.getElementById("switch").innerText = "Want CRT";
+  } else {
+    csssheet.href = "css/global.css";
+    document.getElementById("switch").innerText = "Want MonoChrome";
+  }
+});
